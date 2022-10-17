1bea3e0d91d72f8b9bd8b1bd464835326f36976e
sukumar <108935834+Limitless360@users.noreply.github.com>
2022-10-17 16:48:56 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/auth_test/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example.auth_test;
+
+import android.os.Bundle;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+
+
+public class MainActivity extends FlutterFragmentActivity  {
+}
